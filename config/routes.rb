1a995c49c72abfd51f1efc50373d65711f9fc2a2
san3091 <san3091@gmail.com>
2017-05-06 18:58:48 -0400

    Add lines route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :scenes, only: [:index, :show]
+  resources :lines, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
